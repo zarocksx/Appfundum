7ba340d25ffe91b94df3d9bd3d89b99d98a697c8
Bartoloni Bryan <zarocksx@gmail.com>
2019-12-03 01:31:07 +0100

    added second player

--- scripts/Label.gd
+++ scripts/Label.gd
@@ -11,12 +11,19 @@ func _on_Button_pressed():
 	pic_line();
 	pass;
 
+# P = player | C = cercle | S = second player
 func pic_line():
 	if (line_ready && cercles_ready) == false :
 		line_ready = $json.load_data();
 		cercles_ready = $cercles.load_data();
+	var P = global.random_player();
+	var S = global.random_player();
+	while S == P:
+		randomize();
+		S = global.random_player();
 	var text_final = $json.pick_one();
-	text_final = text_final.format({"P": global.random_player()});
+	text_final = text_final.format({"P": P});
+	text_final = text_final.format({"S": S});
 	text_final = text_final.format({"C": $cercles.pick_one()});
 	set_text(text_final);
 	pass
