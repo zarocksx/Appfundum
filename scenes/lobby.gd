1d8be1feb0c606a46d7841e214186000776d3fbf
Bartoloni Bryan <zarocksx@gmail.com>
2019-12-16 03:24:48 +0100

    anonymous_login firebase

--- scenes/lobby.gd
+++ scenes/lobby.gd
@@ -2,6 +2,10 @@ extends Control
 
 signal show_settings
 
+func _ready():
+    firebase.anonymous_register(get_node("../HTTPRequest"));
+    pass;
+
 func _on_player_added():
 	print("called");
 	print( global.players.size() );
@@ -19,3 +23,12 @@ func _on_player_added():
 func _on_settings_pressed():
 	emit_signal("show_settings");
 	pass # Replace with function body.
+
+
+func _on_HTTPRequest_request_completed(result: int, response_code: int, headers: PoolStringArray, body: PoolByteArray):
+    var response = JSON.parse(body.get_string_from_ascii());
+    if response_code != 200:
+        print("error response : ",response_code);
+    else:
+        print("request completed : ",response);
+    pass # Replace with function body.
--- static/firebase.gd
+++ static/firebase.gd
@@ -3,7 +3,22 @@ extends Node
 const API_KEY = "AIzaSyDSVyawCG-_Fl2-JflunVrZRnlbknAaHKY";
 const PROJECT_ID = "appfundum-1";
 
+const LOGIN_A = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=%s" % API_KEY;
+
+var current_token = "";
+
 var user_info = {};
+
+func _get_token_id_anonymous(result: Array):
+    print(result);
+    return 1;
+
+func anonymous_register(http: HTTPRequest):
+    print(http.request(LOGIN_A, ["Content-Length:0"], false, HTTPClient.METHOD_POST));
+    var result = yield(http, "request_completed") as Array;
+    print(result);
+    if result[1] == 200:
+        current_token = _get_token_id_anonymous(result);
 
 """"
 func save_question(path: String, fields: Dictionary, http: HTTPRequest) :
