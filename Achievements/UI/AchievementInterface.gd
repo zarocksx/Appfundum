c2b7ec2ab294d4c1163ac9dc099b6470d7bd4e03
Bryan Bartoloni <bryan.bartoloni@myskillcamp.com>
2020-10-25 19:20:44 +0100

    add Archievement SDK

---
+++ Achievements/UI/AchievementInterface.gd
@@ -0,0 +1,20 @@
+extends MarginContainer
+
+const ACHIEVEMENT_ITEM = "res://Achievements/UI/AchievementItem.tscn";
+
+var achievementPanel = null;
+var achievementsNodes = {};
+
+func init(achievements):
+	achievementPanel = $Background/Layout/MarginContainer/Panel/MarginContainer/ScrollContainer/VBoxContainer;
+	for i in achievements:
+		var achievement = load(ACHIEVEMENT_ITEM).instance();
+		achievement.set_achievement(achievements[i]);
+		achievementsNodes[i] = achievement;
+		achievementPanel.add_child(achievement);
+
+func _on_Button_pressed():
+	hide();
+
+func _update_bar(achievement_name, achievement):
+	achievementsNodes[achievement_name].update_progress_bar(achievement);
--- scenes/Popup/Blank/Popup.gd
+++ scenes/Popup/Blank/Popup.gd
@@ -2,6 +2,7 @@ extends Popup
 
 
 func _on_Close_pressed():
+	AchievementManager.increment_achievement("randomAchievementExampleOne", 101);
 	self.visible = false
 
 
